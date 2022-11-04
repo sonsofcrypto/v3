063d7142db1f81225c135aa59915cd3fa5952731
web3d3v <shadowyweb3supercoder@protonmail.com>
2022-11-04 12:42:26 +0200

    hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,16 +4,34 @@ import './Hero.css';
 const Hero = (props) => {
     const { progress } = props;
     return (
-        <div className='hero content-wrapper content-wrapper-padding'>
-            <center>
-            <img className='hero-img-lines'
-                 srcSet={
-                     'images2/hero-lines.png 1x, ' +
-                     'images2/hero-lines@2x.png 2x'
-                 }
-                 src={'images2/hero-lines.png'}
-            />
-            </center>
+        <div className='hero-container'>
+            <div className='hero-img-lines'>
+                <div className='hero-img-stars'></div>
+                <div className='hero-img-mountains'></div>
+                <div className='hero content-wrapper content-wrapper-padding'>
+                    <img className='hero-img-palms hero-img-palms-left'
+                         srcSet={
+                             'images2/hero-palms-left.png 1x, ' +
+                             'images2/hero-palms-left@2x.png 2x'
+                         }
+                         src={'images2/hero-palms-left.png'}
+                    />
+                    <img className='hero-img-palms hero-img-palms-right'
+                         srcSet={
+                             'images2/hero-palms-right.png 1x, ' +
+                             'images2/hero-palms-right@2x.png 2x'
+                         }
+                         src={'images2/hero-palms-right.png'}
+                    />
+                    <img className='hero-img-logo'
+                         srcSet={
+                             'images2/hero-logo.png 1x, ' +
+                             'images2/hero-logo@2x.png 2x'
+                         }
+                         src={'images2/hero-logo.png'}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
